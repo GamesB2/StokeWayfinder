b93ad581a168744f149a1ea9604b95529385742c
wcarnall <will.carnall@gmail.com>
2017-06-19 10:15:53 +0100

    Transitions, Geofences started

--- app/src/main/java/com/example/w028006g/regnlogin/Profile.java
+++ app/src/main/java/com/example/w028006g/regnlogin/Profile.java
@@ -1,18 +1,35 @@
 package com.example.w028006g.regnlogin;
 
+import android.app.ActivityOptions;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.BottomNavigationView;
+import android.transition.AutoTransition;
+import android.transition.Explode;
+import android.transition.Scene;
+import android.transition.Transition;
+import android.transition.TransitionManager;
 import android.support.v7.app.AppCompatActivity;
+import android.transition.Slide;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.AccelerateDecelerateInterpolator;
+import android.widget.RelativeLayout;
 
 public class Profile extends AppCompatActivity {
+    private Scene scene1, scene2;
+    //transition to move between scenes
+    private Transition transition;
+    //flag to swap between scenes
+    private boolean start;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
+        getWindow().requestFeature(android.view.Window.FEATURE_CONTENT_TRANSITIONS);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.profile);
 
@@ -22,13 +39,17 @@ public class Profile extends AppCompatActivity {
         MenuItem menuItem = menu.getItem(1);
         menuItem.setChecked(true);
 
+
+
+
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 switch (item.getItemId()){
                     case R.id.ic_map:
                         Intent intent0 = new Intent(Profile.this, MapsActivityNew.class);
-                        startActivity(intent0);
+                        startActivity(intent0, ActivityOptions.makeSceneTransitionAnimation(Profile.this).toBundle());;
+
                         break;
 
                     case R.id.ic_Profile:
@@ -49,4 +70,18 @@ public class Profile extends AppCompatActivity {
             }
         });
     }
+
+    public void changeScene(View v){
+
+        //check flag
+        if(start) {
+            TransitionManager.go(scene2, transition);
+            start=false;
+        }
+        else {
+            TransitionManager.go(scene1, transition);
+            start=true;
+        }
+    }
+
 }
